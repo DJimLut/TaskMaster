317806b695b6b14718496703ead97ebe65f612be
David Jimenez <djimenez@murraystate.edu>
2023-09-28 19:20:00 -0400

    fix header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -4,19 +4,28 @@ function Header() {
 			<nav className="navbar navbar-expand-lg bg-light">
 				<div className="container-fluid">
 					<a className="navbar-brand" href="#">
+						<img
+							src="https://taskmasterstore.com/cdn/shop/files/STAMP-with-Alpha_180x180.png"
+							alt="taskmaster logo"
+						/>
 						TaskMaster
 					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#"
-						aria-controls=""
-						aria-expanded="false"
-						aria-label="Toggle navigation"
+					<div
+						className="collapse navbar-collapse"
+						id="navbarSupportedContent"
 					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a
+									className="nav-link active"
+									aria-current="page"
+									href="#"
+								>
+									Home
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</nav>
 		</div>
