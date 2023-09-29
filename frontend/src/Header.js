3d89d0a09d267be4600a4b8ab06c9425df48eb16
djimenezmsu <djimenez@murraystate.edu>
2023-09-28 20:48:23 -0500

    unwrap header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,34 +1,35 @@
 function Header() {
 	return (
-		<div className="card">
-			<nav className="navbar navbar-expand-lg bg-light">
-				<div className="container-fluid">
-					<a className="navbar-brand" href="#">
-						<img
-							src="https://taskmasterstore.com/cdn/shop/files/STAMP-with-Alpha_180x180.png"
-							alt="taskmaster logo"
-						/>
-						TaskMaster
-					</a>
-					<div
-						className="collapse navbar-collapse"
-						id="navbarSupportedContent"
-					>
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a
-									className="nav-link active"
-									aria-current="page"
-									href="#"
-								>
-									Home
-								</a>
-							</li>
-						</ul>
-					</div>
+		<nav className="navbar navbar-expand-lg text-bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand ml-1 text-bg-light" href="#">
+					<img
+						src="https://taskmasterstore.com/cdn/shop/files/STAMP-with-Alpha_180x180.png"
+						alt="taskmaster logo"
+					/>
+					TaskMaster
+				</a>
+				<button className="navbar-toggler mr-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarSupportedContent"
+				>
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a
+								className="nav-link active ml-1 pb-0"
+								aria-current="page"
+								href="#"
+							>
+								Home
+							</a>
+						</li>
+					</ul>
 				</div>
-			</nav>
-		</div>
+			</div>
+		</nav>
 	);
 }
 
