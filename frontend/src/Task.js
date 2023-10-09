8d52d2a6f85e0c07878288abbecdedc5e7da5f26
djimenezmsu <djimenez@murraystate.edu>
2023-10-08 21:49:33 -0500

    add UI changes

--- frontend/src/Task.js
+++ frontend/src/Task.js
@@ -1,21 +1,29 @@
 const Task = ({ id, title, dueDate, description, status, onEditHandler, onCompleteHandler, onDeleteHandler }) => {
+    const createTask = (id, title, dueDate, description, status) => {
+        return {
+            "id": id,
+            "title": title,
+            "dueDate": dueDate,
+            "description": description,
+            "isComplete": status === "Complete"
+        }
+    };
+
     return(
-        <>
-            <tr>
-                <td>
-                <button type="button" className="btn btn-outline-primary btn-sm mr-1" onClick={() => onEditHandler()}>Edit</button>
-                <button type="button" className="btn btn-outline-primary btn-sm mr-1" onClick={() => onCompleteHandler(id)}>Mark as { status === 'Complete' ? 'Incomplete' : 'Complete' }</button>
-                <button type="button" className="btn btn-outline-primary btn-sm mr-1" onClick={() => onDeleteHandler(id)}>Delete</button>
-                </td>
-                <td>
-                    {title}
-                    <br />
-                    <small className="text-body-secondary">Due: {dueDate.toLocaleString()}</small>
-                </td>
-                <td>{description}</td>
-                <td>{status}</td>
-            </tr>
-        </>
+        <tr>
+            <td>
+            <button type="button" className="btn btn-primary btn-sm mr-1" onClick={() => onEditHandler(createTask(id, title, dueDate, description, status))}>Edit</button>
+            <button type="button" className="btn btn-success btn-sm mr-1" onClick={() => onCompleteHandler(id)}>Mark as { status === 'Complete' ? 'Incomplete' : 'Complete' }</button>
+            <button type="button" className="btn btn-danger btn-sm mr-1" onClick={() => onDeleteHandler(id)}>Delete</button>
+            </td>
+            <td>
+                {title}
+                <br />
+                <small className="text-body-secondary">Due: {dueDate.toLocaleString()}</small>
+            </td>
+            <td>{description}</td>
+            <td>{status}</td>
+        </tr>
     );
 }
 
