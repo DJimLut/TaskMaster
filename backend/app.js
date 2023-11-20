b5af478c8eb2d7ee079d52a91151b0fe805180cf
David Jimenez <djimenez@murraystate.edu>
2023-11-20 18:12:51 -0500

    use express.json instead of bodyParser, update mongo connection url, add SwaggerUI

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const taskRoutes = require("./routes/tasks");
+const express = require("express"),
+	mongoose = require("mongoose"),
+	cors = require("cors"),
+	swaggerJsDoc = require("swagger-jsdoc"),
+	openAPIDefinition = require("./docs/openapi/openapi.json"),
+	swaggerUI = require("swagger-ui-express"),
+	taskRoutes = require("./routes/tasks");
 
 const app = express();
-const port = 3000; // You can choose any available port
+const port = 5000; // You can choose any available port
+
+const options = { definition: openAPIDefinition, apis: ["./routes/*.js"] };
+const specs = swaggerJsDoc(options);
 
 // Connect to MongoDB
 mongoose.connect(
-	"mongodb+srv://service:srvAccPass@taskmastercluster.ct5wrv1.mongodb.net/?retryWrites=true&w=majority",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	}
+	"mongodb+srv://service:srvAccPass@taskmastercluster.ct5wrv1.mongodb.net/?retryWrites=true&w=majority"
 );
 
 // Middleware setup
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // Use task routes
 app.use("/api", taskRoutes);
