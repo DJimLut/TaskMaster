bbdd1ed9ea277a8eac9650c9a7e970ede77a39b6
David Jimenez <djimenez@murraystate.edu>
2023-11-28 23:08:18 -0500

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header.js";
 import "./App.css";
 import TaskList from "./components/TaskList.js";
+import Footer from "./components/Footer.js";
 
 function App() {
 	return (
 		<div className="container-fluid text-bg-light">
 			<Header />
 			<TaskList />
+			<Footer />
 		</div>
 	);
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<footer class="footer">
+			<div class="d-sm-flex justify-content-center">
+				<span class="text-muted d-block text-center text-sm-left d-sm-inline-block">
+					&copy; David Jimenez LLC
+				</span>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
